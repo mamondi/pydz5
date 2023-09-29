fd0e1cda27bbf4358ca6cd28745933035e855e0a
DimamonDESKTOP <dimamon02@gmail.com>
2023-09-29 15:42:50 +0300

    done

---
+++ Lib/1.py
@@ -0,0 +1,26 @@
+num = int(input("Введіть номер місяця[1-12]: "))
+
+if num == 1:
+    print("Січень")
+elif num == 2:
+    print("Лютий")
+elif num == 3:
+    print("Березень")
+elif num == 4:
+    print("Квітень")
+elif num == 5:
+    print("Травень")
+elif num == 6:
+    print("Червень")
+elif num == 7:
+    print("Липень")
+elif num == 8:
+    print("Серпень")
+elif num == 9:
+    print("Вересень")
+elif num == 10:
+    print("Жовтень")
+elif num == 11:
+    print("Листопад")
+elif num == 12:
+    print("Грудень")
