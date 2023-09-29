bea992654ef8c58f88788b6c23b3bf9b6bd4b74d
DimamonDESKTOP <dimamon02@gmail.com>
2023-09-29 15:38:25 +0300

    done

---
+++ Lib/1.py
@@ -0,0 +1,16 @@
+num = int(input("Введіть день тижню[1-7]: "))
+
+if num == 1:
+    print("Понеділок")
+if num == 2:
+    print("Вівторок")
+if num == 3:
+    print("Середа")
+if num == 4:
+    print("Четвер")
+if num == 5:
+    print("П'ятниця")
+if num == 6:
+    print("Субота")
+if num == 7:
+    print("Неділя")
